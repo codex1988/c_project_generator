d0c045aa151dcefde1f1c9e44d2bf9ee865e791e
codex1988 <filimonov.grigoriy.1988@gmail.com>
2025-01-19 10:12:25 +0500

    add flags for more robust code

--- c_project_generator.sh
+++ c_project_generator.sh
@@ -145,7 +145,7 @@ EOL
     cat > "${base_dir}/Makefile" << 'EOL'
 # Compiler settings
 CC = gcc
-CFLAGS = -Wall -Wextra -Wpedantic -std=c11 -Iinclude
+CFLAGS = -Wall -Wextra -Wpedantic -std=c11 -Wconversion -Wshadow -Wfloat-equal -Wformat=2 -Wstrict-overflow=5 -g -Iinclude
 LDFLAGS = -fuse-ld=mold
 
 # Debug settings
